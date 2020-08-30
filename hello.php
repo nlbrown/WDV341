0c830bc45c3d200ad609b761346d34caa7486ce5
nlbrown <42664259+nlbrown@users.noreply.github.com>
2020-08-29 19:04:58 -0500

    Add files via upload

--- hello.php
+++ hello.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html>
 <body>
-
+<div>https://github.com/nlbrown/WDV341/blob/master/hello.php</div>
 <?php
+echo "https://github.com/nlbrown/WDV341/blob/master/hello.php";
 echo "My first PHP script!";
 ?>
 
