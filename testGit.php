6203605be34a8129158937f6044f3cdd09252102
nlbrown <42664259+nlbrown@users.noreply.github.com>
2020-09-12 19:59:26 -0500

    Add files via upload

---
+++ testGit.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    
+<body>
+<div>Github location  https://github.com/nlbrown/WDV341/blob/master/testgit.php</div><br>
+
+<?php
+     static $Norm = "Norman Brown"; 
+    
+     echo "<h1> PHP Test Git Assignment - ". $Norm ." </h1><br>";
+     echo " <p> Define the following Git related terms: </p><br>";
+     echo "<ul>";
+     echo "<li>version control software - Version control allows you to keep track of your work and helps you to easily explore the changes you have made, be it data, coding scripts, notes, etc.  </li>";
+     echo "<li>add - Add README.md to staging area.</li>";
+     echo "<li>commit - Commits changes to files to the local repo. </li> ";
+     echo "<li>push - Sends commited file changes to Github repo.</li>";
+     echo "<li>pull - Retrieve any changes from a Github repo</li>";
+     echo "<li>clone - Creates a local copy of a Github repo. The URL can be copied from Github.com by clicking the `Clone or Download` button.</li>";
+?>
+
+</body>
+</html>
